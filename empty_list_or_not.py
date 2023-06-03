8442e90eb9e22a8b45704e492ece9e3b5a53824b
sagarmakwana04 <133622521+sagarmakwana04@users.noreply.github.com>
2023-06-03 15:36:52 +0530

    3-06-2023

---
+++ empty_list_or_not.py
@@ -0,0 +1,13 @@
+# program to check whether the given list is empty or not
+
+def Enquiry(lis1):
+	if len(lis1) == 0:
+		return 0
+	else:
+		return 1
+
+lis1 = []
+if Enquiry(lis1):
+	print("The list is not empty")
+else:
+	print("Empty List")
