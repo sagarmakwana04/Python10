118b861c13316e4afbf887c7f1ea9ac53632ca7d
sagarmakwana04 <133622521+sagarmakwana04@users.noreply.github.com>
2023-06-07 11:25:28 +0530

    7-6-23

---
+++ file_create_write.py
@@ -0,0 +1,26 @@
+# What is file function ?
+'''A file is a container in computer storage devices used for storing data.
+
+When we want to read from or write to a file, we need to open it first. When we are done, it needs to be closed so that the resources that are tied with the file are freed.
+
+Hence, in Python, a file operation takes place in the following order:
+
+Open a file
+Read or write (perform operation)
+Close the file
+'''
+
+
+# What is keyword to create and write in files
+'''There are two things we need to remember while writing to a file.
+
+If we try to open a file that doesn't exist, a new file is created.
+If a file already exists, its content is erased, and new content is added to the file.
+In order to write into a file in Python, we need to open it in write mode by passing "w" inside open() as a second argument.'''
+
+
+with open('test2.txt', 'w') as file2:
+
+    # write contents to the test2.txt file
+    file2.write('Programming is Fun.\n')
+    file2.write('Python Programming for beginners')
---
+++ read_entire_text_file.py
@@ -0,0 +1,6 @@
+# Python program to read an entire text file
+
+def file_read(fname):
+    txt=open(fname)
+    print(txt.read())
+file_read('test.txt')
