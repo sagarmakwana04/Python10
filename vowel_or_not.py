f4e1868ccc0a6c678a6e791d2c7c7882f01d4ec0
sagarmakwana04 <133622521+sagarmakwana04@users.noreply.github.com>
2023-06-12 19:33:18 +0530

    12/06/2023

--- vowel_or_not.py
+++ vowel_or_not.py
@@ -1,9 +1,10 @@
+# Python Program to Check given letter is vowel or not
 
-n=(input("Enter a letter: "))
+l=(input("Enter a letter: "))
 
 vowel = "aeiouAEIOU"
 
-if n in vowel:
+if l in vowel:
     print("Yes You have entered Vowel")
 else:
     print("No! You have not entered Vowel")
