a9adbc1ca232980ce20c80516bae464371acde54
sagarmakwana04 <133622521+sagarmakwana04@users.noreply.github.com>
2023-06-13 23:24:00 +0530

    13/06/2023

--- common_in_list.py
+++ common_in_list.py
@@ -7,6 +7,6 @@ def commonData (l1,l2):
             if x==y:
                 result = True
                 return result
-
+            
 print(commonData([1,2,3,4,5],[5,6,7,8,9]))
 print(commonData([1,2,3,4,5],[6,7,8,9]))
